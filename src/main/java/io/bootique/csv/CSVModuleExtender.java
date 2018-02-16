2490bf9d1dab4a702569178a1a1d580b457f02f4
Franck Garcia <garcia.franck@gmail.com>
2018-02-16 14:16:15 -0500

    Provide DocumentListener call back

--- src/main/java/io/bootique/csv/CSVModuleExtender.java
+++ src/main/java/io/bootique/csv/CSVModuleExtender.java
@@ -1,38 +1,60 @@
 package io.bootique.csv;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 import com.google.inject.Binder;
+import com.google.inject.Key;
 import com.google.inject.multibindings.Multibinder;
 
 import io.bootique.ModuleExtender;
 
 /**
- * Provides API to contribute custom extensions to {@link CSVModule}. 
+ * Provides API to contribute custom extensions to {@link CSVModule}.
  */
 public class CSVModuleExtender extends ModuleExtender<CSVModuleExtender> {
 
-    private Multibinder<Consumer> simpleListeners;
+    private Multibinder<Consumer<String[]>> rowListeners;
     
+    private Multibinder<Consumer<List<String[]>>> documentListeners;
+
     public CSVModuleExtender(Binder binder) {
         super(binder);
     }
 
     @Override
     public CSVModuleExtender initAllExtensions() {
-        contributeSimpleListeners();
+        contributeRowListeners();
+        contributeDocumentListeners();
         return this;
     }
 
-    public CSVModuleExtender addSimpleListener(Consumer<String[]> lineByLine){
-        contributeSimpleListeners().addBinding().toInstance(lineByLine);
+    /**
+     * Attach a listener that will be called back on every single line read
+     * @param lineByLine the listener
+     * @return this extender instance
+     */
+    public CSVModuleExtender addRowListener(Consumer<String[]> lineByLine){
+        contributeRowListeners().addBinding().toInstance(lineByLine);
         return this;
-        
+    }
+
+    /**
+     * Attach a listener will be called back once when the entire csv document has been read
+     * @param all the listener
+     * @return this extender instance
+     */
+    public CSVModuleExtender addDocumentListener(Consumer<List<String[]>> all){
+        contributeDocumentListeners().addBinding().toInstance(all);
+        return this;
     }
     
-    protected Multibinder<Consumer> contributeSimpleListeners() {
-        return simpleListeners != null ? simpleListeners : (simpleListeners = newSet(Consumer.class));
+    protected Multibinder<Consumer<String[]>> contributeRowListeners() {
+        return rowListeners != null ? rowListeners : (rowListeners = newSet(new Key<Consumer<String[]>>(){}));
     }
     
+    protected Multibinder<Consumer<List<String[]>>> contributeDocumentListeners() {
+        return documentListeners != null ? documentListeners : (documentListeners = newSet(new Key<Consumer<List<String[]>>>(){}));
+    }
 
 }
--- src/main/java/io/bootique/csv/command/CSVParseCommand.java
+++ src/main/java/io/bootique/csv/command/CSVParseCommand.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
 
@@ -24,22 +26,29 @@ import io.bootique.meta.application.CommandMetadata;
 public class CSVParseCommand extends CommandWithMetadata {
     
     private Provider<CSVSettings> csvSettingsProvider;
-    private Provider<Set<Consumer>> listenersProviders;
+    private Provider<Set<Consumer<String[]>>> rowListenersProviders;
+    private Provider<Set<Consumer<List<String[]>>>> documentListenersProviders;
+    
+    private List<String[]> rows = new ArrayList<>();
+    private boolean isDocumentListenerPresent;
     
     @Inject
-    public CSVParseCommand(Provider<CSVSettings> csvSettings, Provider<Set<Consumer>> listeners) {
+    public CSVParseCommand(Provider<CSVSettings> csvSettings, 
+            Provider<Set<Consumer<String[]>>> rowListeners,
+            Provider<Set<Consumer<List<String[]>>>> documentListeners) {
         super(CommandMetadata
                 .builder(CSVParseCommand.class)
                 .description("Parse csv")
                 .build());
         this.csvSettingsProvider = csvSettings;
-        this.listenersProviders = listeners;
+        this.rowListenersProviders = rowListeners;
+        this.documentListenersProviders = documentListeners;
     }
 
     @Override
     public CommandOutcome run(Cli cli) {
         CSVSettings csvSettings = csvSettingsProvider.get();
-        Set<Consumer> listeners = listenersProviders.get();
+        isDocumentListenerPresent = !documentListenersProviders.get().isEmpty();
         //TODO where should we put config param validation ?
         final CSVParser parser = new CSVParserBuilder()
                                         .withSeparator(csvSettings.getSeparator().orElse(ICSVParser.DEFAULT_SEPARATOR))
@@ -48,15 +57,21 @@ public class CSVParseCommand extends CommandWithMetadata {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvSettings.getCsvFilePath()));
              CSVReader csvRead = new CSVReaderBuilder(reader).withCSVParser(parser).build();
                 ){
-            //call back appropriate handler
-            listeners.forEach(csvRead::forEach);
+            //call back listeners interleaving results
+            csvRead.forEach(this::process);
+            documentListenersProviders.get().forEach(l -> l.accept(rows));
         }
         catch (IOException e) {
             return CommandOutcome.failed(-1, e);
         }
         return CommandOutcome.succeeded();
-                
-        
     }
+    
+    private void process(String[] row){
+        rowListenersProviders.get().forEach(l -> l.accept(row));
+        if(isDocumentListenerPresent)
+            rows.add(row);
+    }
+    
 
 }
